4e71039cd8295ff38a504177f519442a6db8e285
Sam <samuel.e.leonard@gmail.com>
2018-08-30 21:20:50 -0400

    added transition to skills, started add lightboxes to projects

--- burger.js
+++ burger.js
@@ -12,12 +12,36 @@ hamBurg.addEventListener('click', function() {
 });
 
 let heading = document.querySelector('.dev__heading');
-let content = document.querySelector('.dev__content');
-let content2 = document.querySelector('.design__content');
-let content3 = document.querySelector('.team__content');
+let dev1 = document.querySelector('.transition-from-left1');
+let dev2 = document.querySelector('.transition-from-left2');
+let dev3 = document.querySelector('.transition-from-left3');
+let dev4 = document.querySelector('.transition-from-left4');
+let dev5 = document.querySelector('.transition-from-left5');
+let des1 = document.querySelector('.transition-from-bottom1');
+let des2 = document.querySelector('.transition-from-bottom2');
+let des3 = document.querySelector('.transition-from-bottom3');
+let des4 = document.querySelector('.transition-from-bottom4');
+let des5 = document.querySelector('.transition-from-bottom5');
+let team1 = document.querySelector('.transition-from-right1');
+let team2 = document.querySelector('.transition-from-right2');
+let team3 = document.querySelector('.transition-from-right3');
+let team4 = document.querySelector('.transition-from-right4');
+let team5 = document.querySelector('.transition-from-right5');
 
 heading.addEventListener('click', function(){
-  content.classList.toggle('transition-from-left');
-  content2.classList.toggle('transition-from-bottom');
-  content3.classList.toggle('transition-from-right');
+  dev1.classList.toggle('transition-from-left1');
+  dev2.classList.toggle('transition-from-left2');
+  dev3.classList.toggle('transition-from-left3');
+  dev4.classList.toggle('transition-from-left4');
+  dev5.classList.toggle('transition-from-left5');
+  des1.classList.toggle('transition-from-bottom1');
+  des2.classList.toggle('transition-from-bottom2');
+  des3.classList.toggle('transition-from-bottom3');
+  des4.classList.toggle('transition-from-bottom4');
+  des5.classList.toggle('transition-from-bottom5');
+  team1.classList.toggle('transition-from-right1');
+  team2.classList.toggle('transition-from-right2');
+  team3.classList.toggle('transition-from-right3');
+  team4.classList.toggle('transition-from-right4');
+  team5.classList.toggle('transition-from-right5');
 });
